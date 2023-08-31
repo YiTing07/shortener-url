a0a56bef25c35279f2f5785f398fff089117ee61
unknown <hevecandy@gmail.com>
2023-08-31 21:05:27 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-// import express
+// 導入 express、 handlebars
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// 設定樣板引擎
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+// 靜態資源
 app.use(express.static('public'))
 
+
+
+// 設定路由
 app.get('/', (req, res) => {
   res.redirect('shortener-url')
 })
+
+app.get('/shortener-url', (req, res) => {
+  res.render('index')
+})
+
+
 
 app.listen(port, (req, res) => {
   console.log(`express server is running on http://localhost:${port}`)
